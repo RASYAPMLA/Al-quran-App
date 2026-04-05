480219d9773a77fcdf361989e9f9295435126f1d
RASYAPMLA <rasyaoktafiandri12@gmail.com>
2026-04-05 21:54:40 +0700

    ulang

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { NavbarComp } from "./components/NavbarComp";
 
-function App() {
+export default function App() {
   return (
     <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-200">
       <NavbarComp />
@@ -11,5 +11,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
